a7c6491d18f7c093247662384eac30056c075644
Hampus Liljekvist <hlilje@users.noreply.github.com>
2014-10-14 15:50:27 +0200

    Add main and reformat

---
+++ Haddockify.java
@@ -0,0 +1,6 @@
+public class Haddockify {
+
+    public static void main(String[] args) {
+        System.out.println("MY HADDOCK");
+    }
+}
--- Matrix.java
+++ Matrix.java
@@ -3,32 +3,32 @@ import java.text.*;
 
 public class Matrix{
 
-	private double myMatrix[][];
+    private double myMatrix[][];
 
     //Takes y-dimension and x-dimension
-	public Matrix(int yDimension, int xDimension){
-		myMatrix = new double[yDimension][xDimension];
-	}
+    public Matrix(int yDimension, int xDimension){
+        myMatrix = new double[yDimension][xDimension];
+    }
 
     //Takes a string as input, with the dimensions at the start and values following
-	public Matrix(String input){
-		Scanner scan = new Scanner(input);
+    public Matrix(String input){
+        Scanner scan = new Scanner(input);
 
-		int yDimension = scan.nextInt();
-		int xDimension = scan.nextInt();
-		
-		myMatrix = new double[yDimension][xDimension];
+        int yDimension = scan.nextInt();
+        int xDimension = scan.nextInt();
 
-		for(int y = 0; y < yDimension; y++){
-			for(int x = 0; x < xDimension; x++){
+        myMatrix = new double[yDimension][xDimension];
 
-				double test = Double.parseDouble(scan.next());
-				myMatrix[y][x] = test;
-			}
-		}
+        for(int y = 0; y < yDimension; y++){
+            for(int x = 0; x < xDimension; x++){
 
-		scan.close();
-	}
+                double test = Double.parseDouble(scan.next());
+                myMatrix[y][x] = test;
+            }
+        }
+
+        scan.close();
+    }
 
     //Inserts value val at index (y, x)
     public void insert(int y, int x, double val){
@@ -37,39 +37,39 @@ public class Matrix{
     }
 
     //Sets value val at index (y, x)
-	public void setValue(int y, int x, double val){
-		if(x < 0 || x >= getX() || y < 0 || y >= getY()){
-			System.err.println("Setting value matrix with wrong dimensions! " +  y + "/" + getY() + " "  + x + "/" + getX());
-			//return;
-		}
-		myMatrix[y][x] = val; 
-		return;
-	}
+    public void setValue(int y, int x, double val){
+        if(x < 0 || x >= getX() || y < 0 || y >= getY()){
+            System.err.println("Setting value matrix with wrong dimensions! " +  y + "/" + getY() + " "  + x + "/" + getX());
+            //return;
+        }
+        myMatrix[y][x] = val;
+        return;
+    }
 
     public double get(int y, int x){
         return getValue(y, x);
     }
 
     //Gets the value at index (y, x)
-	public double getValue(int y, int x){
-		if(x < 0 || x >= getX() || y < 0 || y >= getY()){
-			System.err.println("Getting matrix with wrong dimensions! " +  y + "/" + getY() + " "  + x + "/" + getX());
-			//return 0.0f;
-		}
+    public double getValue(int y, int x){
+        if(x < 0 || x >= getX() || y < 0 || y >= getY()){
+            System.err.println("Getting matrix with wrong dimensions! " +  y + "/" + getY() + " "  + x + "/" + getX());
+            //return 0.0f;
+        }
 
-		return myMatrix[y][x];
-	}
+        return myMatrix[y][x];
+    }
 
 
     //Gets the size of the rows
-	public int getRowSize(){
-		return myMatrix[0].length;
-	}
+    public int getRowSize(){
+        return myMatrix[0].length;
+    }
 
     //Gets the size of the columns
-	public int getColumnSize(){
-		return myMatrix.length;
-	}
+    public int getColumnSize(){
+        return myMatrix.length;
+    }
 
     //Gets the row at index y
     public Matrix getRow(int y){
@@ -139,31 +139,31 @@ public class Matrix{
     }
 
     //Multiples two matrices a and b
-	public static Matrix multiply(Matrix a, Matrix b){
-		if(a.getX() != b.getY()){
-			System.err.println("Can't multiply with those dimensions! " + a.getX() + " " + b.getY());
-			return null;
-		}
+    public static Matrix multiply(Matrix a, Matrix b){
+        if(a.getX() != b.getY()){
+            System.err.println("Can't multiply with those dimensions! " + a.getX() + " " + b.getY());
+            return null;
+        }
 
-		Matrix c = new Matrix(a.getY(), b.getX());
+        Matrix c = new Matrix(a.getY(), b.getX());
 
-		double ans = 0;
+        double ans = 0;
 
-		for(int y = 0; y < a.getY(); y++){
-			for (int x = 0; x < b.getX(); x++ ) {
+        for(int y = 0; y < a.getY(); y++){
+            for (int x = 0; x < b.getX(); x++ ) {
 
-				ans = 0;
+                ans = 0;
 
-				for (int k = 0; k < a.getX(); k++) { // equals b.getY()
-					ans += a.getValue(y, k) * b.getValue(k, x);
-				}
+                for (int k = 0; k < a.getX(); k++) { // equals b.getY()
+                    ans += a.getValue(y, k) * b.getValue(k, x);
+                }
 
-				c.setValue(y, x, ans);
-			}
-		}
+                c.setValue(y, x, ans);
+            }
+        }
 
-		return c;
-	}
+        return c;
+    }
 
     //Returns the sum of all values in the matrix
     public static double sum(Matrix m){
@@ -236,36 +236,36 @@ public class Matrix{
     }
 
     //Returns the matrix as a string
-	public String toString(){
+    public String toString(){
 
         DecimalFormat df = new DecimalFormat("0.0#######"); //df.format(
-		String str = new String();
+        String str = new String();
 
-		for(int y = 0; y < getY(); y++){
-			for(int x = 0; x < getX(); x++){
-				str += getValue(y, x) + " ";
-			}
-		}
+        for(int y = 0; y < getY(); y++){
+            for(int x = 0; x < getX(); x++){
+                str += getValue(y, x) + " ";
+            }
+        }
 
-		return getY() + " " + getX() + " " + str.replace(',', '.') + "\n";
-	}
+        return getY() + " " + getX() + " " + str.replace(',', '.') + "\n";
+    }
 
     //Returns the matrix represented with rows and columns as a string
-	public String toPrettyString(){
+    public String toPrettyString(){
 
         DecimalFormat df = new DecimalFormat("0.00");
-		String str = new String();
+        String str = new String();
 
-		for(int y = 0; y < getY(); y++){
-			for(int x = 0; x < getX(); x++){
-				str += df.format(getValue(y, x)) + "\t";
-			}
+        for(int y = 0; y < getY(); y++){
+            for(int x = 0; x < getX(); x++){
+                str += df.format(getValue(y, x)) + "\t";
+            }
 
-			str += "\n";
-		}
+            str += "\n";
+        }
 
-		return str.replace(',', '.');
-	}
+        return str.replace(',', '.');
+    }
 
     //Fills the matrix with normalized values
     public void fill(){
