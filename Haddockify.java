5e8be464e401db9f62e958dc6c6d0b7a98b4d1fd
Hampus Liljekvist <hlilje@users.noreply.github.com>
2014-10-16 09:27:23 +0200

    Add simple parser

--- Haddockify.java
+++ Haddockify.java
@@ -4,6 +4,16 @@ public class Haddockify {
 
     public static void main(String[] args) {
         TextMap tm = new TextMap();
-        Parser p = new Parser();
+        Parser p = new Parser(tm);
+
+        if (args.length == 0) {
+            System.err.println("You must supply which file to parse");
+            return;
+        } else if (args.length > 1) {
+            System.err.println("Too many arguments");
+            return;
+        } else {
+            p.readFile(args[0]);
+        }
     }
 }
--- Parser.java
+++ Parser.java
@@ -1,3 +1,35 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Parser {
 
+    Scanner sc;
+    TextMap tm;
+    int lineNumber;
+
+    public Parser(TextMap tm) {
+        this.tm = tm;
+        lineNumber = 0;
+    }
+
+    public boolean readFile(String filePath) {
+        try {
+            sc = new Scanner(new File(filePath));
+
+            while (sc.hasNextLine()) {
+                sc = new Scanner(sc.nextLine());
+                ++lineNumber;
+
+                while (sc.hasNext()) {
+                    String word = sc.next().toLowerCase();
+                    tm.put(word, lineNumber);
+                }
+            }
+            sc.close();
+
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
