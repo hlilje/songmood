368499f4ea69d614ffcf9d6302a5a88b8b4d9b77
Hampus Liljekvist <hlilje@users.noreply.github.com>
2014-10-16 09:06:21 +0200

    Work on text map and add Parser class

--- Haddockify.java
+++ Haddockify.java
@@ -1,8 +1,9 @@
 public class Haddockify {
     TextMap tm;
+    Parser p;
 
     public static void main(String[] args) {
         TextMap tm = new TextMap();
-
+        Parser p = new Parser();
     }
 }
--- TextMap.java
+++ TextMap.java
@@ -3,15 +3,30 @@ import java.util.Map;
 
 public class TextMap {
 
-    private static final Map<String, String> textMap;
+    private static final Map<String, Integer> textMap;
 
-    static { textMap = new HashMap<String, String>(); }
+    static { textMap = new HashMap<String, Integer>(); }
 
-    public void put(String key, String value) {
-        textMap.put(key, value);
+    public void put(String value, long line) {
+        String key = createKey(value, line);
+
+        if (!has(value, line))
+            textMap.put(key, 1);
+        else
+            textMap.put(key, textMap.get(key) + 1);
     }
 
-    public boolean has(String key) {
-        return textMap.containsKey(key);
+    public boolean has(String value, long line) {
+        return textMap.containsKey(createKey(value, line));
+    }
+
+    public int count(String value, long line) {
+        if (!has(value, line)) return 0;
+
+        return textMap.get(createKey(value, line));
+    }
+
+    private String createKey(String value, long line) {
+        return line + "::" + value;
     }
 }
